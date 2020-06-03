932be45612095f8634da8a4d359ed044cbba25ec
Tylre Collins <tyler@nhlp.com>
2020-06-02 19:47:41 -0600

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,25 @@
 import React from "react";
+import {Form} from "react-bootstrap";
 
 function Contact() {
     return(
-        <h1>I'm a contact page</h1>
+        <>
+        <h1>Contact</h1>
+         <Form>
+             <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" placeholder="John Doe" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows="5" />
+            </Form.Group>
+         </Form>
+        </>
     )  
 }
 
