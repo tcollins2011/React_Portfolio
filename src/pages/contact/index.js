4cd9381b1d9533dd130d9bfdd44f87118217b814
Tylre Collins <tyler@nhlp.com>
2020-06-16 17:39:35 -0600

    landing page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { MDBAnimation } from "mdbreact";
+import {useSpring, animated} from "react-spring"
 
 const AnimationPage = () => {
+  const props = useSpring({opacity:1, from: {opacity:0}})
     return (
-        <MDBAnimation type="bounce" infinite>
-            <img className="img-fluid" alt="" src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png" />
-        </MDBAnimation>
+      <animated.div style={props}>I will fade in</animated.div>
     );
 };
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,43 +1,41 @@
 import React from "react";
-import './home.css';
-import { MDBView, MDBMask, MDBRow, MDBContainer,MDBAnimation} from "mdbreact";
+import "./home.css";
+import { useSpring, animated } from "react-spring";
+import NavBar from "../components/Nav";
 
 function Home() {
-    return(
-        <div id='homePage' className='bg'>
-            <MDBView>
-                <MDBMask className="d-flex justify-content-center align-items-center gradient">
-                    <MDBContainer>
-                        <MDBRow>
-                            <MDBAnimation
-                                type="fadeInLeft"
-                                delay=".3s"
-                                className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
-                                >
-                                <h1 className="h1-responsive font-weight-bold">
-                                    Full Stack Web Developer
-                                </h1>
-                                <hr className="hr-light" />
-                                <h6 className="mb-4">
-                                    React | Javascript | Express | MySQL | MongoDB | Python | Bootstrap 
-                                </h6>
-                                <div className ='socialLinks'>
-                                    <a href='https://www.linkedin.com/in/tyler-collins-0a5ab6162/' rel='noopener noreferrer' target='blank'>
-                                        <i className=' fab fa-linkedin' aria-hidden='true'/>
-                                    </a>
-                                    <a href='https://github.com/tcollins2011' target='blank'>GitHub</a>
-                                </div>
-                            </MDBAnimation>
-                        </MDBRow>
-                        <MDBAnimation type="bounce" infinite>
-                                <img className="img-fluid" alt="" src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png" />
-                            </MDBAnimation>
-                    </MDBContainer>
-                </MDBMask>
-            </MDBView>
+  const props = useSpring({
+    marginTop: "25vh",
+    from: { marginTop: "-25vh" },
+    delay: 100,
+  });
+  return (
+    <div className="bg">
+      <NavBar></NavBar>
+      <animated.div style={props} className="bannerText">
+        <h1>Full Stack Web Developer</h1>
+        <hr />
+        <p>
+          React | Javascript | Express | MySQL | MongoDB | Python | Bootstrap
+        </p>
+        <div className="socialLinks">
+          <a
+            href="https://www.linkedin.com/in/tyler-collins-0a5ab6162/"
+            target="blank"
+          >
+            Linkedin
+          </a>
+          <a
+            className="link"
+            href="https://github.com/tcollins2011"
+            target="blank"
+          >
+            GitHub
+          </a>
         </div>
-    )
+      </animated.div>
+    </div>
+  );
 }
-
 
 export default Home;
